0b577d7f5db6cc7a723960a690291a28273bd02f
Jonathan Zacsh <jzacsh@gmail.com>
2017-09-29 00:57:31 -0400

    noop(readability) bail early, and forget

--- hexint_test.go
+++ hexint_test.go
@@ -21,13 +21,15 @@ func Test_IsValidHex(t *testing.T) {
 	for _, tc := range tt {
 		t.Run(string(tc.Given), func(it *testing.T) {
 			isValid := IsHex(tc.Given)
-			if isValid != tc.IsValid {
-				validity := "in"
-				if tc.IsValid {
-					validity = ""
-				}
-				it.Fatalf("expected '%c' to be %svalid", tc.Given, validity)
+			if isValid == tc.IsValid {
+				return
 			}
+
+			validity := "in"
+			if tc.IsValid {
+				validity = ""
+			}
+			it.Fatalf("expected '%c' to be %svalid", tc.Given, validity)
 		})
 	}
 }
